81da54e099206ce9c5d0e63c4aeaf656f0ba8d09
tofita <tofita1337@gmail.com>
2019-08-24 23:26:25 +0300

    test

---
+++ assets/js/app.js
@@ -0,0 +1,57 @@
+$(function() {
+
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    /* Header fixed*/
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() { 
+       scrollOffset = $(this).scrollTop();
+
+       checkScroll(scrollOffset);
+    });
+
+
+    function checkScroll(scrollOffset){
+        if(scrollOffset >= introH) {
+            header.addClass("fixed");
+       } else{
+        header.removeClass("fixed");
+       }
+    }
+
+
+    /* smooth scroll */
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+    
+    /* menu nav toggle*/
+    $("#nav_toggle").on("click", function(event){
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+
+
+
+
+    });
+
+
+});
